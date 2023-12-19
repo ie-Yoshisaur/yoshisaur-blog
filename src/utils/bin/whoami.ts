50e28b907d7f82b7deb2a8bb77e454fbf6466332
e205723 <e205723@ie.u-ryukyu.ac.jp>
2023-12-20 06:44:52 +0900

    change into simpler content

--- src/utils/bin/whoami.ts
+++ src/utils/bin/whoami.ts
@@ -31,14 +31,6 @@ I can speak English because I spent 3 years in Canada 🇨🇦 during high schoo
   - A relational database written in Rust.
 
 
-<a href="/">yoshisaur.net</a>
-
-  - My personal terminal-like website.
-  - I forked this from <a href="https://term.m4tt72.com/">m4tt72</a> and modified it under the MIT license.
-  - I have modified it to be able to post blogs by writing md files.
-
----
-
 Type 'help' to see list of available commands.
 `;
 };
